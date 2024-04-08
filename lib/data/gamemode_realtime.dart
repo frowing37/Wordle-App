6e4cc6c6c10ed09656c8a97ab31a570e271a46dd
frowing37 <froward37@gmail.com>
2024-04-09 02:23:52 +0300

    readItem() bug fixed and update gamrooms ui

--- lib/data/gamemode_realtime.dart
+++ lib/data/gamemode_realtime.dart
@@ -9,28 +9,32 @@ class Gamemode_RT {
   DatabaseReference _dbRef = FirebaseDatabase.instance.ref().child("gameMode/");
 
   Future<List<gameMode>> readItems() async {
+  Completer<List<gameMode>> completer = Completer<List<gameMode>>();
   List<gameMode> gameModes = [];
-  print("BBBBBBBBBB");
-  var subscription = await _dbRef.onValue.listen((event) {
-    print("AAAAAAAAAAAAAAAAAAA");
-    var json = event.snapshot.value;
-    if (json != null && json is Map<String,dynamic>) {
-      // Veritabanından gelen veriyi gameMode nesnelerine dönüştürme
-      json.forEach((key, value) {
-        gameMode mode = gameMode(
-          value["name"],
-          value["letterCount"],
-          value["user1"],
-          value["user2"],
-        );
-        gameModes.add(mode);
-      });
-    }
-  });
-  // Veritabanı dinleyicisini temizleme
-  subscription.cancel();
-
-  return gameModes;
+  try {
+    var subscription;
+    subscription = _dbRef.onValue.listen((event) {
+      var json = event.snapshot.value;
+      if (json != null && json is Map) {
+        json.forEach((key, value) {
+          gameMode mode = gameMode(
+            value["name"],
+            value["letterCount"],
+            value["user1"],
+            value["user2"],
+          );
+          gameModes.add(mode);
+        });
+        // İşlem tamamlandıktan sonra aboneliği iptal et
+        subscription.cancel();
+        completer.complete(gameModes);
+      }
+    });
+  } catch (error) {
+    print(error);
+    return gameModes;
+  }
+  return completer.future;
 }
 
   void addItem(Map<String, dynamic> data) async {
--- lib/ui/gamerooms_ui.dart
+++ lib/ui/gamerooms_ui.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:app4/data/gamemode_realtime.dart';
 import 'package:app4/model/gameMode.dart';
 import 'package:app4/model/userData.dart';
@@ -25,15 +27,39 @@ class activeGameRooms extends State<activeRooms> {
   @override
   void initState() {
     super.initState();
-    _loadGameModes();
+    _startPeriodicRefresh();
   }
 
-  _loadGameModes() async {
-    List<gameMode> modes = await rt.readItems();
-    setState(() {
-      gameModes = modes;
+  void _loadGameModes() {
+    rt.readItems().then((List<gameMode> modes) {
+      setState(() {
+        gameModes = modes;
+      });
+    }).catchError((error) {
+      print("Error loading game modes: $error");
     });
   }
+
+  @override
+  void dispose() {
+    // Timer'ı dispose edin
+    _stopPeriodicRefresh();
+    super.dispose();
+  }
+
+  void _startPeriodicRefresh() {
+    // Timer'ı başlatın
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      // Her 2 saniyede bir UI'yı yenileyin
+      _loadGameModes();
+    });
+  }
+
+  void _stopPeriodicRefresh() {
+    // Timer'ı durdurun
+    // Bu aslında gerekli değil, çünkü StatefulWidget dispose olduğunda zaten otomatik olarak durdurulur
+  }
+
 
 
   void createRoom() {
@@ -43,63 +69,99 @@ class activeGameRooms extends State<activeRooms> {
       );
   }
 
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Aktif Odalar', style: TextStyle(fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.white,
-      ),
-      body: Column(
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Aktif Odalar', style: TextStyle(fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+    ),
+    body: Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-      Container(
-      padding: EdgeInsets.only(top: 7,bottom: 7),
-      decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                border: Border.all(color: Colors.white),
-                                gradient: LinearGradient(colors:[Color.fromRGBO(137, 235, 234, 0.8),Color.fromRGBO(50, 168, 82, 0.8)])
-                              ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(padding: EdgeInsets.only(left: 10)),
-              Text("Kullanıcı ID: ", style: TextStyle(fontSize: 17.0,fontWeight: FontWeight.bold, color: Colors.white)),
-              Text("${userData.uid}", style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.bold, color: Colors.white))
-            ]
+        Container(
+          padding: EdgeInsets.only(top: 7,bottom: 7),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            border: Border.all(color: Colors.white),
+            gradient: LinearGradient(colors:[Color.fromRGBO(137, 235, 234, 0.8),Color.fromRGBO(50, 168, 82, 0.8)])
           ),
-          SizedBox(height: 5),
-          Row(
+          child: Column(
             children: [
-              Padding(padding: EdgeInsets.only(left: 10)),
-              Text("Kullanıcı Adı: ${userData.displayName}", style: TextStyle(fontSize: 17.0,fontWeight: FontWeight.bold, color: Colors.white))
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(left: 10)),
+                  Text("Kullanıcı ID: ", style: TextStyle(fontSize: 17.0,fontWeight: FontWeight.bold, color: Colors.white)),
+                  Text("${userData.uid}", style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.bold, color: Colors.white))
+                ]
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(left: 10)),
+                  Text("Kullanıcı Adı: ${userData.displayName}", style: TextStyle(fontSize: 17.0,fontWeight: FontWeight.bold, color: Colors.white))
+                ]
+              ),
             ]
-          ),
-        ]
-      )
-    ),
-    SizedBox(height: 30),
-    gameModes.isEmpty 
-      ? Center(child: Text("Aktif oda yok"))
-      : ListView.builder(
-          shrinkWrap: true,
-          itemCount: gameModes.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Oyun Modu: ${gameModes[index].name}'),
-              subtitle: Text('Harf Sayısı: ${gameModes[index].letterCount}\nOda Kurucusu: ${gameModes[index].user1}'),
-            );
-          }
+          )
         ),
         SizedBox(height: 30),
-    ElevatedButton(
-      onPressed: (){ createRoom(); },
-      child: Text("Oda Oluştur",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ,color: Color.fromRGBO(50, 168, 82, 1)))
-    )
-  ]
-)
+        gameModes.isEmpty 
+          ? Center(child: Text("Aktif oda yok"))
+          : Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(), // ListView içindeki kaydırmayı devre dışı bırakır
+                      itemCount: gameModes.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+  margin: EdgeInsets.symmetric(vertical: 5.0),
+  padding: EdgeInsets.all(10.0),
+  decoration: BoxDecoration(
+    border: Border.all(color: Colors.white),
+    borderRadius: BorderRadius.circular(10.0),
+    gradient: LinearGradient(colors: [Color.fromRGBO(95, 163, 163, 0.7),Color.fromRGBO(32, 110, 53, 0.8)])
+  ),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('${gameModes[index].name}', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,color: Colors.white)),
+            SizedBox(height: 2.0),
+            Text('Harf Sayısı: ${gameModes[index].letterCount}', style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold, color: Colors.white)),
+            Text('Oda Kurucusu: ${gameModes[index].user1}', style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold, color: Colors.white)),
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {}, 
+        child: Icon(Icons.play_arrow,color: Colors.green, size: 35),
+        style: ElevatedButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15)),
+      ),
+    ],
+  ),
 );
-  }
+                      }
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(padding: EdgeInsets.only(bottom: 40),
+            child:ElevatedButton(
+                      onPressed: (){ createRoom(); },
+                      child: Text("Oda Oluştur",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ,color: Color.fromRGBO(50, 168, 82, 1)))
+                    ))
+      ],
+    ),
+  );
+}
 
 }
